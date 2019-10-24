17e85882677dd3001a2b66f7b7b582a6cf940b7d
codeproj01 <cliftonmpls@comcast.net>
2019-10-23 21:37:59 -0500

    code update

--- models/cart.js
+++ models/cart.js
@@ -15,3 +15,26 @@ module.exports = function Cart(nCart) {
       keptItem.price = keptItem.item.price * keptItem.qty;
       this.totalQty++;
       this.totalPrice += keptItem.item.price;
+  };
+  this.reduceByOne = function(id) {
+      this.items[id].qty--;
+      this.items[id].price -= this.items[id].item.price;
+      this.totalQty--;
+      this.totalPrice -= this.items[id].item.price;
+      if (this.items[id].qty <= 0) {
+          delete this.items[id];
+      }
+  };
+  this.removeItem = function(id) {
+      this.totalQty -= this.items[id].qty;
+      this.totalPrice -= this.items[id].price;
+      delete this.items[id];
+  };
+  this.generateArray = function() {
+      let arr = [];
+      for (let id in this.items) {
+          arr.push(this.items[id]);
+      }
+      return arr;
+  };
+};
