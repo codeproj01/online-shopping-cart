4b89afabcc66ac61312ec2737fda7caa87bb8d1b
codeproj01 <cliftonmpls@comcast.net>
2019-10-23 21:34:27 -0500

    code update

--- models/cart.js
+++ models/cart.js
@@ -1,40 +1,17 @@
-module.exports = function Cart(oldCart) {
-  this.items = oldCart.items || {};
-  this.totalQty = oldCart.totalQty || 0;
-  this.totalPrice = oldCart.totalPrice || 0;
+module.exports = function Cart(nCart) {
+  this.items = nCart.items || {};
+  this.totalQty = nCart.totalQty || 0;
+  this.totalPrice = nCart.totalPrice || 0;
   this.add = function(item, id) {
-      let storedItem = this.items[id];
-      if (!storedItem) {
-          storedItem = this.items[id] = {
+      let keptItem = this.items[id];
+      if (!keptItem) {
+          keptItem = this.items[id] = {
               item: item,
               qty: 0,
               price: 0
           };
       }
-      storedItem.qty++;
-      storedItem.price = storedItem.item.price * storedItem.qty;
+      keptItem.qty++;
+      keptItem.price = keptItem.item.price * keptItem.qty;
       this.totalQty++;
-      this.totalPrice += storedItem.item.price;
-  };
-  this.reduceByOne = function(id) {
-      this.items[id].qty--;
-      this.items[id].price -= this.items[id].item.price;
-      this.totalQty--;
-      this.totalPrice -= this.items[id].item.price;
-      if (this.items[id].qty <= 0) {
-          delete this.items[id];
-      }
-  };
-  this.removeItem = function(id) {
-      this.totalQty -= this.items[id].qty;
-      this.totalPrice -= this.items[id].price;
-      delete this.items[id];
-  };
-  this.generateArray = function() {
-      let arr = [];
-      for (let id in this.items) {
-          arr.push(this.items[id]);
-      }
-      return arr;
-  };
-};
+      this.totalPrice += keptItem.item.price;
