cd6abece9ea623156c10d317ddba5de4438687a4
Brian Bartholomew <me@bcbrian.com>
2019-10-28 13:36:39 -0500

    fixed react part and remove node modules

--- component/App.js
+++ component/App.js
@@ -1,34 +1,30 @@
-import { confirmAlert } from 'react-confirm-alert'; // Import
-import 'react-confirm-alert/src/react-confirm-alert.css'; // Import css
-import Modal from "./Modal";
-import Confirm from "./Confirm";
+import React from "react";
+import ReactDOM from "react-dom";
+import { confirmAlert } from "react-confirm-alert"; // Import
+import "react-confirm-alert/src/react-confirm-alert.css"; // Import css
 
 class App extends React.Component {
-  submit = () => {
+  submit() {
     confirmAlert({
-      title: 'Confirm to submit',
-      message: 'Are you sure to do this.',
+      title: "Cancel",
+      message: "Are you sure to do this.",
       buttons: [
         {
-          label: 'Yes',
-          onClick: () => alert('Click Yes')
+          label: "Yes",
+          onClick: () => window.location.href = "/"
         },
         {
-          label: 'No',
-          onClick: () => alert('Click No')
+          label: "No",
+          onClick: () => {}
         }
       ]
     });
   };
- 
+
   render() {
-    return (
-      <div className='container'>
-        <button onClick={this.submit}>Confirm dialog</button>
-        <Modal />
-      </div>
-    );
+    return <button type="button" class="btn btn-danger" onClick={this.submit}>Cancel</button>;
   }
 }
 
-export default App;
+const rootElement = document.getElementById("cancel-button");
+ReactDOM.render(<App />, rootElement);
